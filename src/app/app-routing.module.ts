21dd6212650e217f59bf05ece3724edc88d94851
tatay2019 <my.work.20199@gmail.com>
2021-01-23 00:28:50 +0100

    hostCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NavBarComponent} from './Components/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NavBarComponent
+  },
+  {
+    path: '**',
+    component: NavBarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
